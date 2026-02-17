17b5272bdd3037f2061fa6e40edcb0907ca65ce0
vampiriawendy <wendymarvel871@gmail.com>
2026-02-16 20:06:50 -0500

    Basic package set up

---
+++ Runtime/TestPackageBehavior.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace com.rachel.rachelTestPackage
+{
+    public class TestPackageBehavior : MonoBehaviour
+    {
+        public void TestingFunction()
+        {
+            print("Code from my package");
+        }
+    
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
